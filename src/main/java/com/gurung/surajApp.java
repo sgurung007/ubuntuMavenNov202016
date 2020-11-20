4cc25e06781b7a3abb34cea3c984ce7bb38d1e39
suraj gurung <sgurung007>
2020-11-20 00:38:21 -0600

    second commit

--- src/main/java/com/gurung/surajApp.java
+++ src/main/java/com/gurung/surajApp.java
@@ -3,5 +3,6 @@ package com.gurung;
 public class surajApp {
     public static void main(String[] args) {
         System.out.println("this is fucking awesome");
+        System.out.println("this is my second line");
     }
 }
